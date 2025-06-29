108aa441dd0be0d6afcfa7951e07803024a9f6a3
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-06-29 23:04:58 +0200

    Add validate function

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -3,7 +3,22 @@ import type { Table } from "../table";
 
 export const table = <T extends StandardSchemaV1>(
   name: string,
-  schema: StandardSchemaV1.InferInput<T>,
+  schema: T,
 ): Table => {
-  return { name };
+  const validate = async <T extends StandardSchemaV1>(
+    input: StandardSchemaV1.InferInput<T>,
+  ): Promise<StandardSchemaV1.InferOutput<T>> => {
+    let result = schema["~standard"].validate(input);
+    if (result instanceof Promise) result = await result;
+
+    // if the `issues` field exists, the validation failed
+    if (result.issues) {
+      throw new Error(JSON.stringify(result.issues, null, 2));
+    }
+
+    return result.value;
+  };
+  return {
+    name,
+  };
 };
--- packages/table.ts
+++ packages/table.ts
@@ -1,3 +1,4 @@
 export type Table = {
   name: string;
+  validate: object;
 };
