39137b3cc1cf06ba9baf426c2290d912e37aeb15
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-06-29 23:41:51 +0200

    Semi-working types

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,24 +1,30 @@
 import type { StandardSchemaV1 } from "@standard-schema/spec";
-import type { Table } from "../table";
+import type { Table } from "./table";
 
 export const table = <T extends StandardSchemaV1>(
   name: string,
   schema: T,
-): Table => {
-  const validate = async <T extends StandardSchemaV1>(
+): Table<T> => {
+  const validate = async (
     input: StandardSchemaV1.InferInput<T>,
-  ): Promise<StandardSchemaV1.InferOutput<T>> => {
+  ): Promise<
+    | { status: "success"; value: StandardSchemaV1.InferOutput<T> }
+    | { status: "error"; error: Error }
+  > => {
     let result = schema["~standard"].validate(input);
     if (result instanceof Promise) result = await result;
 
-    // if the `issues` field exists, the validation failed
     if (result.issues) {
-      throw new Error(JSON.stringify(result.issues, null, 2));
+      return {
+        status: "error",
+        error: new Error(JSON.stringify(result.issues, null, 2)),
+      };
     }
 
-    return result.value;
+    return { status: "success", value: result.value };
   };
   return {
+    validate,
     name,
   };
 };
--- packages/upstash-redis/index.ts
+++ packages/upstash-redis/index.ts
@@ -1,16 +1,28 @@
-import type { Table } from "../table";
+import type { StandardSchemaV1 } from "@standard-schema/spec";
+import type { Table } from "./table";
 import { Redis, type RedisConfigNodejs } from "@upstash/redis";
 
 export const upstashRedisAdapter = (config: RedisConfigNodejs) => {
   const redis = new Redis(config);
 
-  return (table: Table) => {
+  return <T extends StandardSchemaV1>(table: Table<T>) => {
     return {
-      async get(id: string) {
+      async get(id: string): Promise<
+        | {
+            status: "error";
+            error: Error;
+          }
+        | {
+            status: "success";
+            value: StandardSchemaV1.InferOutput<T>;
+          }
+      > {
         const key = `${table.name}:${id}`;
-        return await redis.get(key);
+        const result = await redis.get(key);
+        const validatedResult = await table.validate(result);
+        return validatedResult;
       },
-      async set(id: string, value: any) {
+      async set(id: string, value: StandardSchemaV1.InferOutput<T>) {
         const key = `${table.name}:${id}`;
         await redis.set(key, value);
       },
